44c8cece18e4948c52c1210a3e4327c5e25c6afc
Forbidden <lloduardo@gmail.com>
2023-11-11 11:30:17 -0500

    First commit in 'App for eyeglass repair business.'

---
+++ android/app/src/main/kotlin/l1dro/com/glasses_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package l1dro.com.glasses_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const cPrincipalColor = Color(0x00F9200D);
+const cBackgroundColor = Color(0x00FCBF92);
+const cBackgroundColors = Color(0xFFF8F8F8);
+const cTextColor = Color(0xFF222B45);
