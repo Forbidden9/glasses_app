8ac9bff0cd9ba3d02364e844c814e0565f4fc764
Forbidden <lloduardo@gmail.com>
2023-11-12 21:10:41 -0500

    [Main] - Add navigation bar in app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'pages/fee/fee.dart';
+import 'pages/location/location.dart';
+import 'pages/personal_information/personal_information.dart';
+import 'pages/stock/stock.dart';
+import 'pages/tips/tips.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,12 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'L&L Reparador de Espejuelos',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, background: Colors.green[800]),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'L&L Reparador de Espejuelos'),
     );
   }
 }
@@ -30,40 +37,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _currentpage = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<Widget> _pages = const [
+    Fee(),
+    Stock(),
+    PersonalInformation(),
+    Location(),
+    Tips()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        backgroundColor: Colors.lightBlue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _pages[_currentpage],
+      bottomNavigationBar:
+      BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _currentpage = index;
+          });
+        },
+        currentIndex: _currentpage,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: "Tarifas"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.remove_red_eye),
+              label: "Inventario"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Contacto"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.pin_drop),
+              label: "Ubicación"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: "Consejos")
+        ]
+      )
     );
   }
 }
