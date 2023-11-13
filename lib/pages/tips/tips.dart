6c008dba1f69ea7d8750ce602511941f056460d2
Forbidden <lloduardo@gmail.com>
2023-11-12 21:14:22 -0500

    [Tips] - Added tips page.

---
+++ lib/pages/tips/tips.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Tips extends StatelessWidget {
+  const Tips({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Consejos para espejuelos"),
+    );
+  }
+}
