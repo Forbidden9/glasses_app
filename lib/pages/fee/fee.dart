2d4f54af24cff880c97ebc1437f6c63c96cff08c
Forbidden <lloduardo@gmail.com>
2023-11-12 21:11:30 -0500

    [Fee] - Added fee page.

---
+++ lib/pages/fee/fee.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Fee extends StatelessWidget {
+  const Fee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Tarifa de servicios"),
+    );
+  }
+}
