ec1a6fd1bb3f25b3cf45f0c48375f2184c3c120d
Forbidden <lloduardo@gmail.com>
2023-11-14 16:38:52 -0500

    [Colors] - Fixed Scaffold color and appBar color.

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 const cPrincipalColor = Color(0x00F9200D);
-const cBackgroundColor = Color(0x00FCBF92);
-const cBackgroundColors = Color(0xFFF8F8F8);
+const aBackgroundColor = Color.fromARGB(228, 237, 107, 31);
+const sBackgroundColor = Color(0xFFF8F8F8);
 const cTextColor = Color(0xFF222B45);
