0dcacfd91f4bb47887c1dbf365b3592f899047ba
Forbidden <lloduardo@gmail.com>
2023-11-17 18:59:26 -0500

    [Services optic] - Added list of the optic services.

--- lib/pages/fee/optica_fee.dart
+++ lib/pages/fee/optica_fee.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:glasses_app/pages/fee/armor/armor.dart';
+import 'package:glasses_app/pages/fee/consult/consult.dart';
+import 'package:glasses_app/pages/fee/cristal/cristal.dart';
+import 'package:glasses_app/utils/colors.dart';
 
 class OpticaFee extends StatelessWidget {
   const OpticaFee({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: sBackgroundColor,
+      body: Animate(
+        effects: const [
+          FadeEffect(),
+          SlideEffect()
+        ],
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.all(5),
+          children: const <Widget>[
+            Text("Servicios de consultas", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+            ConsultWidget(),
+            Text("Venta de armaduras", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+            ArmorWidget(),
+            Text("Venta de cristales", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+            CristalWidget(),
+            ],
+        ),
+      ),
+    );
   }
 }
