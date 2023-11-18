9d4179c21bd80da324a0dcc0e6531aa6ba2f8b25
Forbidden <lloduardo@gmail.com>
2023-11-18 01:52:24 -0500

    [Colors] - Added background color (navigator, buttons) and edited background color (scaffold)

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 const cPrincipalColor = Color(0x00F9200D);
 const aBackgroundColor = Color.fromARGB(228, 237, 107, 31);
-const sBackgroundColor = Color(0xFFF8F8F8);
-const cTextColor = Color(0xFF222B45);
+const sBackgroundColor = Colors.white;
+const bBackgroundColor = Colors.white;
+const nColor = Color.fromARGB(255, 227, 64, 14);
+const cTextColor = Colors.black;
