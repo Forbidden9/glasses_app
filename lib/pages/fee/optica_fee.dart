e1a5f5df9e9532c2fc3c1c5a5266470fd111f00e
Forbidden <lloduardo@gmail.com>
2023-11-18 02:31:43 -0500

    [Optic Fee] - Fixed dependencies in optic fee page.

--- lib/pages/fee/optica_fee.dart
+++ lib/pages/fee/optica_fee.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:glasses_app/pages/fee/armor/armor.dart';
-import 'package:glasses_app/pages/fee/consult/consult.dart';
-import 'package:glasses_app/pages/fee/cristal/cristal.dart';
 import 'package:glasses_app/pages/fee/data_fee.dart';
 import 'package:glasses_app/utils/colors.dart';
 
