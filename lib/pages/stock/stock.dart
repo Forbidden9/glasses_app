1fb06e7ce7c6dec5cbbc01dd99c01982bd786154
Forbidden <lloduardo@gmail.com>
2023-11-18 02:30:04 -0500

    [Stock] - Fixed stock page.

--- lib/pages/stock/stock.dart
+++ lib/pages/stock/stock.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:gap/gap.dart';
+import 'package:glasses_app/pages/stock/armor/armor.dart';
+import 'package:glasses_app/pages/stock/cristal/cristal.dart';
+import 'package:glasses_app/utils/colors.dart';
 
 class Stock extends StatelessWidget {
   const Stock({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Inventario"),
+    return Scaffold(
+      backgroundColor: sBackgroundColor,
+      body: Animate(
+        effects: const [
+          FadeEffect(),
+          SlideEffect()
+        ],
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          padding: const EdgeInsets.only(left: 15, right: 5),
+          children: const <Widget>[
+            // Gap(5),
+            // Text("Servicios de consultas", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22,)),
+            // Gap(5),
+            // ConsultWidget(),
+            Gap(5),
+            Text("Galenos", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+            Gap(5),
+            GalenoWidget(),
+            Gap(5),
+            Text("Cristales", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+            Gap(5),
+            CristalWidget(),
+            ],
+        ),
+      ),
     );
   }
 }
