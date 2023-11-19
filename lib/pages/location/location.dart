c30e27a4cef00bbfcee8dc6c89b13239f8019617
Forbidden <lloduardo@gmail.com>
2023-11-18 21:43:31 -0500

    [Location] - Added map location.

--- lib/pages/location/location.dart
+++ lib/pages/location/location.dart
@@ -1,12 +1,46 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Location extends StatelessWidget {
-  const Location({super.key});
+import 'package:flutter/material.dart';
+import 'package:glasses_app/utils/colors.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class LocationPage extends StatefulWidget {
+  const LocationPage({super.key});
+
+  @override
+  State<LocationPage> createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  static const LatLng _goggleLocalization = LatLng(23.135791, -82.3758943);
+  static const LatLng localDestination = LatLng(23.1377247,-82.3564536);
+  
+  final Completer<GoogleMapController> _controller = Completer();
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+}
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Ubicación y Horarios"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ubicación", style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: aBackgroundColor,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: _goggleLocalization,
+          zoom: 14
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId("localDestination"),
+            position: localDestination
+          )
+        },
+        onMapCreated: _onMapCreated,
+      ),
     );
   }
 }
