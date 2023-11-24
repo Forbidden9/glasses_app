f299b29dbb9e89ba4f71923455d96730543b061e
Forbidden <lloduardo@gmail.com>
2023-11-24 02:22:23 -0500

    [Contact Screen] - Fixed with this screen.

--- lib/pages/personal_information/personal_information.dart
+++ lib/pages/personal_information/personal_information.dart
@@ -2,6 +2,7 @@ import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:glasses_app/pages/location/location.dart';
 import 'package:glasses_app/utils/social.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -13,59 +14,96 @@ class PersonalInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: sBackgroundColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Bloomix", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-          const SizedBox(height: 20),
-          Image.asset("lib/images/background/logo.jpg", height: 240, width: 240,),
-          const SizedBox(height: 20),
-          const Text("Horario", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-          const SizedBox(height: 20),
-          const Text("Lunes - Sábado", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
-          const SizedBox(height: 20),
-          const Text("9:00 - 16:00", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: psBackgroundColor,
+      body: Center(
+        child: Container(
+          child: Column(
             children: [
-              IconButton(
-                onPressed: () {
-                  sendMessageWhatsapp();
-                },
-                icon: Image.asset("lib/images/social/whatsapp_icon.png", height: 45, width: 45,),
-                tooltip: "Whatsapp",
+              const Gap(120),
+              Container(
+                height: 430,
+                child: Image.asset("lib/images/background/logo.png")
               ),
-              IconButton(
-                onPressed: () {
-                  sendMessageTelegram();
-                },
-                icon: Image.asset("lib/images/social/telegram_icon.png", height: 40, width: 40,),
-                tooltip: "Telegram",
-              ),
-              IconButton(
-                onPressed: () {
-                  sendMessageInstagram();
-                },
-                icon: Image.asset("lib/images/social/instagram_icon.png", height: 35, width: 35,),
-                tooltip: "Instagram",
-              ),
-              IconButton(
-                onPressed: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LocationPage()
-                    )
+              const Gap(100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        sendMessageWhatsapp();
+                      },
+                      icon: Image.asset("lib/images/social/whatsapp_icon.png", height: 45, width: 45,),
+                      tooltip: "Whatsapp",
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        sendMessageTelegram();
+                      },
+                      icon: Image.asset("lib/images/social/telegram_icon.png", height: 40, width: 40,),
+                      tooltip: "Telegram",
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        sendMessageInstagram();
+                      },
+                      icon: Image.asset("lib/images/social/instagram_icon.png", height: 35, width: 35,),
+                      tooltip: "Instagram",
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        redirectYoutube();
+                      },
+                      icon: Image.asset("lib/images/social/youtube_icon.png", height: 45, width: 54,),
+                      tooltip: "Youtube",
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: IconButton(
+                      onPressed: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LocationPage()
+                          )
+                        )
+                      },
+                      icon: Image.asset("lib/images/social/location_map_icon.jpg", height: 35, width: 35,),
+                      tooltip: "Ubicación",
+                    ),
                   )
-                },
-                icon: Image.asset("lib/images/social/location_map_icon.jpg", height: 35, width: 35,),
-                tooltip: "Ubicación",
+                ],
               )
             ],
-          )   
-        ],
-      )
+          ),
+        ),
+      ),
     );
   }
 }
@@ -95,6 +133,19 @@ void sendMessageTelegram() async {
     await launchUrl(Uri.parse(url), mode: mode);
   } else {
     throw Exception("Telegram could not launching url");
+  }
+}
+
+void redirectYoutube() async{
+  String urlWeb = "https://youtube.com/$pYoutube";
+  LaunchMode mode = LaunchMode.externalApplication;
+
+  if (kIsWeb|| Platform.isAndroid || Platform.isIOS) {
+    if (await canLaunchUrl(Uri.parse(urlWeb))) {
+      await launchUrl(Uri.parse(urlWeb), mode: mode);
+    }
+  } else {
+    throw 'There was a problem to open the url';
   }
 }
 
