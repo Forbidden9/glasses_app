5d60c75fa55423b89790ff5b6a65d4c7bf309d4c
Forbidden <lloduardo@gmail.com>
2023-11-24 22:45:08 -0500

    [Utils] - Added const values in color and social files.

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,3 +6,4 @@ const sBackgroundColor = Colors.white;
 const bBackgroundColor = Colors.white;
 const nColor = Color.fromARGB(255, 227, 64, 14);
 const cTextColor = Colors.black;
+const psBackgroundColor = Color.fromARGB(255, 239, 235, 235);
--- lib/utils/social.dart
+++ lib/utils/social.dart
@@ -2,3 +2,4 @@
   String cPhone = "+5358285255";
   String gTelegram = "httpstmeU9eIMtdB0fRyubt0";
   String pInstagram = "lyl_repairglasess";
+  String pYoutube = "@leandrooduardo4137?si=GLJcP4VGNxsKvs1v";
